d8a1e5cfe3e4a9405a2dc6da1e365c8f99be87bf
Nathan-Liu-Tyne <nathan.liu@tyne-engineering.com>
2022-01-25 09:55:09 -0500

    Initial implementation

---
+++ random.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int random_int(int max)
+{
+	return rand()%max;
+}
+
+int main(int argc, char *argv[]) 
+{
+	if (argc != 2) {
+		return EXIT_FAILURE;
+	}
+}
